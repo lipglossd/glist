04abe8f329be1d8535a13ad532074148f9f075b0
denezgo <>
2026-05-29 00:17:06 +0300

    .

--- index.js
+++ index.js
@@ -33,44 +33,44 @@ app.get('/plist', async (req, res) => {
         }
 
         const plistContent = `<?xml version="1.0" encoding="UTF-8"?>
-        <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-        <plist version="1.0">
-        <dict>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+    <dict>
         <key>items</key>
         <array>
             <dict>
-            <key>assets</key>
-            <array>
+                <key>assets</key>
+                <array>
+                    <dict>
+                        <key>kind</key>
+                        <string>software-package</string>
+                        <key>url</key>
+                        <string>${await getDirectUrl(plistInfo.ipa_url)}</string>
+                    </dict>
+                    <dict>
+                        <key>kind</key>
+                        <string>display-image</string>
+                        <key>needs-shine</key>
+                        <false/>
+                        <key>url</key>
+                        <string>https://lipglossd.github.io/${plistInfo.img_url}</string>
+                    </dict>
+                </array>
+                <key>metadata</key>
                 <dict>
-                <key>kind</key>
-                <string>software-package</string>
-                <key>url</key>
-                <string>${await getDirectUrl(plistInfo.ipa_url)}</string>
+                    <key>bundle-identifier</key>
+                    <string>${plistInfo.bundleId.trim()}</string>
+                    <key>bundle-version</key>
+                    <string>${plistInfo.version.split(' ')[0].trim()}</string>
+                    <key>kind</key>
+                    <string>software</string>
+                    <key>title</key>
+                    <string>${plistInfo.title.trim()}</string>
                 </dict>
-                <dict>
-                <key>kind</key>
-                <string>display-image</string>
-                <key>needs-shine</key>
-                <false/>
-                <key>url</key>
-                <string>https://lipglossd.github.io/${plistInfo.img_url}</string>
-                </dict>
-            </array>
-            <key>metadata</key>
-            <dict>
-                <key>bundle-identifier</key>
-                <string>${plistInfo.bundleId.trim()}</string>
-                <key>bundle-version</key>
-                <string>${plistInfo.version.split(' ')[0].trim()}</string>
-                <key>kind</key>
-                <string>software</string>
-                <key>title</key>
-                <string>${plistInfo.title.trim()}</string>
-            </dict>
             </dict>
         </array>
-        </dict>
-        </plist>`.trim()
+    </dict>
+</plist>`.trim()
         
         res.setHeader('Content-Type', 'text/xml; charset=utf-8')
         res.send(plistContent)
